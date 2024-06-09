6d94e3e3ada43d97e59d428ead2dea0c4d92bb56
saudlari <larissa@riseup.net>
2024-06-09 23:49:57 +0200

    Add chakra to nextjs. Firsts Pages.

---
+++ src/app/home/page.jsx
@@ -0,0 +1,10 @@
+'use client'
+import { Link } from '@chakra-ui/next-js'
+
+export default function Page() {
+  return (
+    <>
+    te quiero
+   </>
+  )
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+'use client'
+import ContactList from '@/components/ContactList'
+// import { Link } from '@chakra-ui/next-js'
+
+export default function Page() {
+  return (
+    <ContactList />
+  )
+}
+
